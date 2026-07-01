727202254aea9932603a964b1874090a74720294
Magura <fahimmia017740@gmail.com>
2026-07-01 19:21:29 +0600

    build: disable PNG crunching for release build
    Reduces build time by skipping PNG optimization during the release packaging process.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,7 @@ android {
 
   buildTypes {
     release {
-      isCrunchPngs = true
+      isCrunchPngs = false
       isMinifyEnabled = true
       isShrinkResources = true
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
